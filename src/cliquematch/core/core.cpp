9ea1bec8a209ac95a2597eca0743178c2e4b47c8
ahgamut <41098605+ahgamut@users.noreply.github.com>
2020-05-06 04:24:56 +0530

    do I need L2A if I have A2L??

--- src/cliquematch/core/core.cpp
+++ src/cliquematch/core/core.cpp
@@ -4,7 +4,6 @@ namespace py = pybind11;
 void init_pygraph(py::module&);
 void init_A2Agraph(py::module&);
 void init_A2Lgraph(py::module&);
-void init_L2Agraph(py::module&);
 void init_L2Lgraph(py::module&);
 void init_Aligngraph(py::module&);
 
@@ -14,7 +13,6 @@ PYBIND11_MODULE(core, m)
     init_A2Agraph(m);
     init_A2Lgraph(m);
     init_L2Lgraph(m);
-    init_L2Agraph(m);
     init_Aligngraph(m);
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
