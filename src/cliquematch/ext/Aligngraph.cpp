a23ef40b326d597a74371cdbaaee0859afdd35f7
ahgamut <41098605+ahgamut@users.noreply.github.com>
2020-05-21 02:53:29 +0530

    fixed aligngraph coordinate system error, now everyone is (col, row)

--- src/cliquematch/ext/Aligngraph.cpp
+++ src/cliquematch/ext/Aligngraph.cpp
@@ -38,9 +38,11 @@ double inline filter_score(Eigen::Ref<DoubleMatrixR> control,
     rot_con.rowwise() += c;
 
     RCU = rot_con.cast<std::size_t>();
+    // assumption is M1, M2, control_pts and therefore RCU all are in the same
+    // coordinate system, i.e. (x,y) or (col, row)
     for (k = 0; k < static_cast<std::size_t>(rot_con.rows()); k++)
     {
-        if (RCU(k, 0) < m_r && RCU(k, 1) < m_c && msk(RCU(k, 0), RCU(k, 1)))
+        if (RCU(k, 1) < m_r && RCU(k, 0) < m_c && msk(RCU(k, 1), RCU(k, 0)))
             msk_score++;
     }
     msk_score /= rot_con.rows();
