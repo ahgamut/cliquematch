351c01bb70b9d0a1027bb5b416a20b96f64c9465
ahgamut <41098605+ahgamut@users.noreply.github.com>
2020-06-27 17:48:26 +0530

    example applications

---
+++ examples/ccmm.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+"""
+    Example application of cliquematch, an implementation of the
+    CCMM algorithm described in:
+
+    San Segundo, Pablo, and Jorge Artieda. "A novel clique formulation for the
+    visual feature matching problem." Applied Intelligence 43.2 (2015):
+    325-342.
+
+    Download the dinosaur dataset from here:
+        https://www.robots.ox.ac.uk/~vgg/data/mview/
+"""
+import cv2
+import numpy as np
+import cliquematch
+from matplotlib import pyplot as plt
+from matplotlib.patches import Circle, ConnectionPatch
+
+
+class ImageData(object):
+    surf = cv2.xfeatures2d.SURF_create(2000)
+
+    def __init__(self, path, flags=0):
+        self._path = path
+        self.data = cv2.imread(path)
+        kps, desc = self.surf.detectAndCompute(self.data, None)
+        self.pts = np.float64(cv2.KeyPoint.convert(kps))
+        self.desc = desc
+
+    def __str__(self):
+        return "{}: {}, {} keypoints, {} descriptors".format(
+            self._path, self.data.shape, len(self.pts), self.desc.shape[1]
+        )
+
+    def plot(self, axis):
+        axis.set_yticks([])
+        axis.set_xticks([])
+        axis.imshow(self.data[:, :, [2, 1, 0]])
+        return axis
+
+
+def get_Kbest_indices(S1, S2, K):
+    indices = np.zeros((len(S1), len(S2)), np.bool)
+    for i in range(len(S1)):
+        s1i = S1[i, :]
+        s2mod = np.sum((S2 - s1i) ** 2, 1)
+        indices[i, np.argpartition(s2mod, K)[:K]] = True
+    return indices
+
+
+def view_correspondence(img1, img2):
+    ind = get_Kbest_indices(img1.desc, img2.desc, 3)
+
+    def condition_func(P, i1, j1, Q, i2, j2):
+        return ind[i1, i2] & ind[j1, j2]
+
+    G = cliquematch.A2AGraph(img1.pts, img2.pts)
+    G.epsilon = 50
+    G.build_edges_with_condition(condition_func, False)
+    G.use_heuristic = True
+    G.use_dfs = False
+    print(G)
+    corr = G.get_correspondence(False)
+    clique_size = len(corr[0])
+
+    fig = plt.figure()
+    axs = fig.subplots(1, 2)
+
+    img1.plot(axs[0])
+    img2.plot(axs[1])
+
+    for i in range(clique_size):
+        axs[0].add_artist(Circle(corr[0][i], color="red", radius=1.5))
+        axs[1].add_artist(Circle(corr[1][i], color="red", radius=1.5))
+        axs[1].add_artist(
+            ConnectionPatch(
+                xyA=corr[1][i],
+                xyB=corr[0][i],
+                coordsA="data",
+                coordsB="data",
+                axesA=axs[1],
+                axesB=axs[0],
+                color="red",
+                alpha=0.4,
+            )
+        )
+
+    plt.show()
+    plt.close()
+
+
+def main():
+    img1 = ImageData("viff.000.ppm")
+    print(img1)
+    img2 = ImageData("viff.002.ppm")
+    print(img2)
+    view_correspondence(img1, img2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/molecule.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Molecule structure matching based on inter-atomic distances,
+    using the algorithm described in:
+
+    Gardiner, Eleanor J., Peter J. Artymiuk, and Peter Willett.
+    "Clique-detection algorithms for matching three-dimensional molecular
+    structures." Journal of Molecular Graphics and Modelling 15.4 (1997):
+    245-253.
+
+    The bzr_3d.sd dataset was obtained from:
+
+    Sutherland, Jeffrey J., Lee A. O'brien, and Donald F. Weaver.
+    "Spline-fitting with a genetic algorithm: A method for developing
+    classification structure− activity relationships." Journal of chemical
+    information and computer sciences 43.6 (2003): 1906-1915.
+
+"""
+import numpy as np
+import cliquematch
+from rdkit.Chem import PandasTools, Draw
+from matplotlib import pyplot as plt
+
+
+class MolData(object):
+    def __init__(self, mol):
+        self._mol = mol
+        self._mol.Compute2DCoords()
+        self.positions = np.array(self._mol.GetConformer().GetPositions())
+
+
+def condition_closure(M1, M2):
+    def condition(P, i1, j1, Q, i2, j2):
+        b1 = M1.GetBondBetweenAtoms(i1, j1)
+        b2 = M2.GetBondBetweenAtoms(i2, j2)
+        if b1 is None and b2 is None:
+            return True
+        elif b1 is not None and b2 is not None:
+            return ~(b1.IsInRing() ^ b2.IsInRing())
+        else:
+            return False
+
+    return condition
+
+
+def main():
+    mols = PandasTools.LoadSDF("./bzr_3d.sd")
+    data = [MolData(x) for x in mols["ROMol"]]
+    plot(data[0], data[1], "comp_0_1.png")
+
+
+def plot(d1, d2, filename=None):
+    G = cliquematch.A2AGraph(d1.positions, d2.positions)
+    cfunc = condition_closure(d1._mol, d2._mol)
+    G.epsilon = 1e-6
+    G.use_heuristic = True
+    G.build_edges_with_condition(cfunc, False)
+    sub1, sub2 = G.get_correspondence(return_indices=True)
+    fig = plt.figure(figsize=(12.1, 6.1))
+    axs = fig.subplots(1, 2)
+    img0 = Draw.MolToImage(
+        d1._mol,
+        size=(600, 600),
+        highlightAtoms=sub1.tolist(),
+        highlightMap={int(x): (0.05 * i, 0.7, 0) for i, x in enumerate(sub1)},
+    )
+    axs[0].imshow(np.array(img0))
+    axs[0].set_xticks([])
+    axs[0].set_yticks([])
+    img1 = Draw.MolToImage(
+        d2._mol,
+        size=(600, 600),
+        highlightAtoms=sub2.tolist(),
+        highlightMap={int(x): (0.05 * i, 0.7, 0) for i, x in enumerate(sub2)},
+    )
+    axs[1].imshow(np.array(img1))
+    axs[1].set_xticks([])
+    axs[1].set_yticks([])
+    fig.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
+    fig.savefig(filename)
+    plt.close()
+    print(
+        len(d1.positions), len(d2.positions), G.n_edges, len(G.get_max_clique()),
+    )
+
+
+if __name__ == "__main__":
+    main()
