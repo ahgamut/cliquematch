61ab51ba7e134377637a482b44e0d8ea4602b91d
ahgamut <41098605+ahgamut@users.noreply.github.com>
2021-02-24 19:14:16 +0530

    fix error in installation of twine for upload

--- travis/upload.sh
+++ travis/upload.sh
@@ -2,11 +2,12 @@
 echo "travis tag was originally $TRAVIS_TAG"
 TRAVIS_TAG=$(git tag -l --contains HEAD)
 echo "git cmd gave $TRAVIS_TAG"
+python3 -m pip install importlib-metadata>=1
+python3 -m pip install twine
 if [[ -z "$TRAVIS_TAG" ]]; then
 	echo "No tag; not uploading to PyPI"
 else
 	echo "Uploading $TRAVIS_TAG to PYPI via twine..."
-	python3 -m pip install twine
 	python3 -m twine check wheelhouse/*.whl
 	python3 -m twine upload --skip-existing wheelhouse/*.whl
 fi
